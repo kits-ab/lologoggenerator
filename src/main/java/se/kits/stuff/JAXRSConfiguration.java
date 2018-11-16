4dcd27f1b452ce55ec765e67c427c39f2618249b
David Chang <david.chang@kits.se>
2018-11-16 16:13:44 +0100

    add endpoint to write to text to file

---
+++ src/main/java/se/kits/stuff/JAXRSConfiguration.java
@@ -0,0 +1,9 @@
+package se.kits.stuff;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class JAXRSConfiguration extends Application {
+
+}
