eb13f527ca61f26cda218a76a2242f29981dbbc2
David Chang <david.chang@kits.se>
2018-12-04 15:43:53 +0100

    Change Web Access Log preset name/key

--- src/main/java/se/kits/stuff/model/Presets.java
+++ src/main/java/se/kits/stuff/model/Presets.java
@@ -18,7 +18,7 @@ public class Presets {
         LinkedHashMap<String, String> presetMap = new LinkedHashMap<>();
         presetMap.put("Example Java EE format", "%date{ISO8601} [%thread] %-5level %logger{32} - %msg%n");
         presetMap.put("Logstash Encoder", "{\"@timestamp\": \"%date{yyyy-MM-dd}T%date{HH:mm:ss.SSS}\", \"@version\": int, \"message\": %msg, \"logger_name\": %logger, \"thread_name\": %thread \"level\": %-5level, \"level_value\": %level, \"stack_trace\": \"string\", \"X-Span-Id\": \"string\"}%n");
-        presetMap.put("web access log (nginx default format)", "$remote_addr - $remote_user [%date{dd/MMM/yyyy:HH:mm:ss Z}] \"$request\" $status $body_bytes_sent \"$http_referer\" \"$http_user_agent\"");
+        presetMap.put("Web Access Log (Combined Log Format)", "$remote_addr - $remote_user [%date{dd/MMM/yyyy:HH:mm:ss Z}] \"$request\" $status $body_bytes_sent \"$http_referer\" \"$http_user_agent\"");
         presetMap.put("Custom Log Pattern", CUSTOM_LOG_PATTERN_STRING);
         return presetMap;
     }
