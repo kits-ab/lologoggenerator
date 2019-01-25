1681d4d3c0ae9bf542b1f756bc449c8f60f38422
David Chang <david.chang@kits.se>
2019-01-25 17:00:03 +0100

    disable log pattern presets which don't have functionality

--- src/main/java/se/kits/stuff/model/Presets.java
+++ src/main/java/se/kits/stuff/model/Presets.java
@@ -14,11 +14,11 @@ public class Presets {
     @LogPreset
     private LinkedHashMap<LogPatternPresetKey, String> buildLogPatternPresets() {
         LinkedHashMap<LogPatternPresetKey, String> presetMap = new LinkedHashMap<>();
-        presetMap.put(LogPatternPresetKey.WILDFLY, "%-24date{\"yyyy-MM-dd HH:mm:ss,SSS\"} %-5level [%logger] \\(%thread\\) WFLYSOMETHING0017: %msg%n");
+//        presetMap.put(LogPatternPresetKey.WILDFLY, "%-24date{\"yyyy-MM-dd HH:mm:ss,SSS\"} %-5level [%logger] \\(%thread\\) WFLYSOMETHING0017: %msg%n");
         presetMap.put(LogPatternPresetKey.SPRING, "$timestamp $logLevel $processId --- [$thread] $loggerName : $logMessage");
-        presetMap.put(LogPatternPresetKey.LOGSTASH_ENCODER, "{\"@timestamp\": \"%date{yyyy-MM-dd}T%date{HH:mm:ss.SSS}\", \"@version\": int, \"message\": %msg, \"logger_name\": %logger, \"thread_name\": %thread \"level\": %-5level, \"level_value\": %level, \"stack_trace\": \"string\", \"X-Span-Id\": \"string\"}%n");
+//        presetMap.put(LogPatternPresetKey.LOGSTASH_ENCODER, "{\"@timestamp\": \"%date{yyyy-MM-dd}T%date{HH:mm:ss.SSS}\", \"@version\": int, \"message\": %msg, \"logger_name\": %logger, \"thread_name\": %thread \"level\": %-5level, \"level_value\": %level, \"stack_trace\": \"string\", \"X-Span-Id\": \"string\"}%n");
         presetMap.put(LogPatternPresetKey.WEB_ACCESS_LOG, "$remote_addr - $remote_user [$time_local] \"$request\" $status $body_bytes_sent \"$http_referer\" \"$http_user_agent\"");
-        presetMap.put(LogPatternPresetKey.CUSTOM_PATTERN, CUSTOM_LOG_PATTERN_VALUE);
+//        presetMap.put(LogPatternPresetKey.CUSTOM_PATTERN, CUSTOM_LOG_PATTERN_VALUE);
         return presetMap;
     }
 }
